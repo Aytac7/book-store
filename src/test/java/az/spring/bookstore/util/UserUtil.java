0343c267400d1908de222cffe521869cc839ac86
Aytac7 <aramazanli@std.beu.edu.az>
2024-04-30 14:06:58 +0500

    userUtil added

--- src/test/java/az/spring/bookstore/util/UserUtil.java
+++ src/test/java/az/spring/bookstore/util/UserUtil.java
@@ -1,6 +1,8 @@
 package az.spring.bookstore.util;
 
 import az.spring.bookstore.entity.UserEntity;
+import az.spring.bookstore.request.create.CreateUserRequest;
+import az.spring.bookstore.response.create.CreateUserResponse;
 
 public class UserUtil {
 
@@ -14,4 +16,26 @@ public class UserUtil {
                 .surname("test_surname")
                 .build();
    }
+
+    public static CreateUserResponse userResponse(){
+        return CreateUserResponse.builder()
+                .name("test_name")
+                .surname("test_surname")
+                .build();
+    }
+
+    public static CreateUserRequest userRequest(){
+        return CreateUserRequest.builder()
+                .name("test_name")
+                .surname("test_surname")
+                .build();
+    }
+
+    public static UserEntity saveUserEntity(){
+        return UserEntity.builder()
+                .name("test_name")
+                .surname("test_surname")
+                .fkLibraryId("1")
+                .build();
+    }
 }
