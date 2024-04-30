8d77b5f3da7cf1eb77a08cc5d6d5c78adc8bbd5e
Aytac7 <aramazanli@std.beu.edu.az>
2024-04-30 19:09:30 +0500

    commit

--- src/main/java/az/spring/bookstore/controller/LibraryController.java
+++ src/main/java/az/spring/bookstore/controller/LibraryController.java
@@ -1,18 +1,22 @@
 package az.spring.bookstore.controller;
 
+import az.spring.bookstore.entity.BookEntity;
+import az.spring.bookstore.mapper.BookMapper;
+import az.spring.bookstore.repository.LibraryRepository;
+import az.spring.bookstore.request.add.LibraryBookIdAddRequest;
 import az.spring.bookstore.request.create.CreateLibraryRequest;
 import az.spring.bookstore.request.read.LibrarySpecificReadRequest;
+import az.spring.bookstore.response.create.CreateBookResponse;
 import az.spring.bookstore.response.create.CreateLibraryResponse;
 import az.spring.bookstore.response.read.LibraryReadResponse;
 import az.spring.bookstore.service.create.CreateLibraryService;
+import az.spring.bookstore.service.delete.BookDelete.DeleteFkBookIdLibraryService;
+import az.spring.bookstore.service.read.libraryRead.GetAllBooksFromLibrary;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,15 +24,42 @@ import java.util.List;
 @RequestMapping("/libraries")
 @RequiredArgsConstructor
 public class LibraryController {
-    private final CreateLibraryService createService;
+    private final CreateLibraryService libraryService;
+    private final LibraryRepository libraryRepository;
+    private final DeleteFkBookIdLibraryService deleteFkBookIdLibraryService;
+    private final GetAllBooksFromLibrary getAllBooksFromLibrary;
+    private final BookMapper bookMapper;
 
     @PostMapping("/create")
     public ResponseEntity<CreateLibraryResponse> create(@RequestBody @Valid CreateLibraryRequest createRequest){
-        return ResponseEntity.status(HttpStatus.CREATED).body(createService.create(createRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryService.create(createRequest));
+    }
+    @PostMapping("/add/book")
+    public ResponseEntity<LibraryBookIdAddRequest> addBookIdToLibrary(@RequestBody @Valid LibraryBookIdAddRequest libraryBookIdAddRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryService.addBookId(libraryBookIdAddRequest));
     }
 
-    @PostMapping("/read")
+
+    @PostMapping("/read/users")
     public ResponseEntity<List<LibraryReadResponse>> create(@RequestBody @Valid LibrarySpecificReadRequest request){
-        return ResponseEntity.status(HttpStatus.OK).body(createService.readAllSpecificLibrary(request));
+        return ResponseEntity.status(HttpStatus.OK).body(libraryService.readAllSpecificLibrary(request));
     }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteBookById(@RequestBody LibraryBookIdAddRequest libraryBookIdAddRequest){
+        deleteFkBookIdLibraryService.deleteFkBookId(libraryBookIdAddRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping("/{libraryId}")
+    public CreateBookResponse getBooksByLibrary(@PathVariable Long libraryId) {
+        BookEntity book=getAllBooksFromLibrary.getBookByLibrary(libraryId);
+        return  bookMapper.mapToBookResponse(book);
+    }
+
+
+
+
+
 }
