3579d094e9ba2ef3bfc8ec42293a8ad700f2a08c
Aytac7 <aramazanli@std.beu.edu.az>
2024-04-30 14:44:21 +0500

    BookController

--- src/main/java/az/spring/bookstore/controller/BookController.java
+++ src/main/java/az/spring/bookstore/controller/BookController.java
@@ -1,10 +1,12 @@
 package az.spring.bookstore.controller;
-import az.spring.bookstore.exception.IdNotFoundException;
-import az.spring.bookstore.repository.BookRepository;
+
 import az.spring.bookstore.request.create.CreateBookRequest;
 import az.spring.bookstore.response.create.CreateBookResponse;
 import az.spring.bookstore.service.create.CreateBookService;
-import az.spring.bookstore.service.read.GetAllBookService;
+import az.spring.bookstore.service.delete.BookDelete.DeleteBookService;
+import az.spring.bookstore.service.read.bookRead.GetAllBookService;
+import az.spring.bookstore.service.read.bookRead.GetByIdBookService;
+import az.spring.bookstore.service.update.BookUpdate.UpdateBookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,30 +20,39 @@ import java.util.List;
 public class BookController {
     private final CreateBookService createBookService;
     private final GetAllBookService getAllBookService;
-    private final BookRepository bookRepository;
+
+    private final GetByIdBookService getByIdBookService;
+    private final DeleteBookService deleteBookService;
+    private  final UpdateBookService updateBookService;
 
 
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<?> add(@RequestBody CreateBookRequest createBookRequest){
          createBookService.saveBook(createBookRequest);
-        return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
+        return new ResponseEntity<>("Book created successfully", HttpStatus.CREATED);
 
     }
 
-    @GetMapping
-    public List<CreateBookResponse> getData() {
+    @GetMapping("/getAll")
+    public List<CreateBookResponse> getAllBooks() {
         return  getAllBookService.getAll();
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<CreateBookResponse> update(@PathVariable Long id,
+                                                     @RequestBody CreateBookRequest bookRequest){
+
+        return updateBookService.update(id,bookRequest);
+    }
+
 
     @DeleteMapping
-    public ResponseEntity<?> deleteData(Long id){
-        if(bookRepository.existsById(id)) {
-            bookRepository.deleteById(id);
-            return new ResponseEntity<>("Book is deleted successfully", HttpStatus.NO_CONTENT);
-        }
-        else
-          throw new IdNotFoundException(HttpStatus.BAD_REQUEST.name(), "Id dont exist");
+    public ResponseEntity<?> deleteBook(@RequestParam Long id){
+        return deleteBookService.deleteBook(id);
+    }
 
+    @GetMapping("/getBookById")
+    public CreateBookResponse getBook(@RequestParam Long id){
+        return getByIdBookService.getById(id);
     }
 }
