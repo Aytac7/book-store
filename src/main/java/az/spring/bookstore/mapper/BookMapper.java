89257e6f0e9d67ccf6e95f6a5babc52a80f4c4b6
Aytac7 <aramazanli@std.beu.edu.az>
2024-04-30 14:46:53 +0500

    mappers commit

--- src/main/java/az/spring/bookstore/mapper/BookMapper.java
+++ src/main/java/az/spring/bookstore/mapper/BookMapper.java
@@ -4,18 +4,20 @@ import az.spring.bookstore.entity.BookEntity;
 import az.spring.bookstore.request.create.CreateBookRequest;
 import az.spring.bookstore.response.create.CreateBookResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE,
+@Mapper(componentModel = "spring",nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE,
         unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface BookMapper {
 
     BookEntity mapToBookEntity(CreateBookRequest bookRequest);
     CreateBookResponse mapToBookResponse(BookEntity bookEntity);
+
+    void mapForUpdate( @MappingTarget BookEntity bookEntity ,CreateBookRequest bookRequest );
 
     List<CreateBookResponse> mapToListResponse(List<BookEntity> bookEntityList);
 
--- src/main/java/az/spring/bookstore/mapper/LibraryMapper.java
+++ src/main/java/az/spring/bookstore/mapper/LibraryMapper.java
@@ -1,20 +1,26 @@
 package az.spring.bookstore.mapper;
 import az.spring.bookstore.entity.LibraryEntity;
+import az.spring.bookstore.request.add.LibraryBookIdAddRequest;
 import az.spring.bookstore.request.create.CreateLibraryRequest;
 import az.spring.bookstore.response.create.CreateLibraryResponse;
 import az.spring.bookstore.response.read.LibraryReadResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, unmappedSourcePolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface  LibraryMapper {
 
 
     LibraryEntity mapToLibraryEntity(CreateLibraryRequest libraryRequest);
+    LibraryEntity mapForBookIdLibraryEntity(LibraryBookIdAddRequest libraryBookIdAddRequest);
 
     CreateLibraryResponse mapToLibraryResponse(LibraryEntity libraryEntity);
+    @Mapping(target = "libraryId",source = "id")
+    LibraryBookIdAddRequest mapForBookIdLibraryResponse(LibraryEntity libraryEntity);
 
     //TODO: bizim userId gore axtardigimiz kitablarin mentigine uygun olaraq bunu
     // yaziram normalda danismisiq ki bir userin sadece bir kitabxanasi olsun
